c7719dbd541516f38207e1bf83f0d39efdc7b61c
azharanowar <azharanowar@gmail.com>
2022-09-07 02:01:12 +0600

    Adding new player to list function completed with validation and condition appy

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,21 @@
-document.getElementById("playersCard").addEventListener('click', function( event ) {
-    const isSelectBtnClicked = event.target.classList.contains("select-btn");
-    if ( isSelectBtnClicked ) {
-       const getPlayerName = event.target.parentNode.childNodes[1].innerText;
-        console.log(getPlayerName)
-    }
+document.getElementById("playersCard").addEventListener('click', function(event) {
+    playersList(event);
 });
+
+function playersList(event) {
+    const isSelectBtnClicked = event.target.classList.contains("select-btn");
+    const totalPlayersAdded = document.getElementById("playersListShow").childNodes.length;
+    if (isSelectBtnClicked) {
+        if (totalPlayersAdded < 5) {
+            const getPlayerName = event.target.parentNode.childNodes[1].innerText;
+            const newPlayerListTag = document.createElement('li');
+            newPlayerListTag.innerHTML = "<span>" + getPlayerName + "</span>";
+            document.getElementById("playersListShow").appendChild(newPlayerListTag);
+            event.target.innerText = "Selected";
+            event.target.disabled = true;
+        } else {
+            alert("You've already added 5 players. To add a new player you have to remove a player from selected lists!!!");
+        }
+        
+    }
+}
